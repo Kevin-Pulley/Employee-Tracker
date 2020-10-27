f0215eb9e92ff6bdb9ce8354265b86cfb73ffbb9
Kevin Pulley <skippy1025@gmail.com>
2020-10-27 17:41:10 -0500

    Got update employee working, started add roll function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-const { start } = require("repl");
+const cTable = require("console.table");
 
-const connection = my.sql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -12,69 +12,152 @@ const connection = my.sql.createConnection({
 
 connection.connect(function (err) {
   if (err) throw err;
-  start();
+  startPrompt();
 });
 
-function start() {
+function startPrompt() {
   inquirer
     .prompt({
-      name: "start",
+      name: "choice",
       type: "list",
       message: "Select one option below",
       choices: [
         "View All Employees",
-        "View All Employees by Department",
-        "View All Employees by Manager",
+        "View All Employee's By Roles",
+        "View all Employee's By Departments",
+        "Update Employee",
         "Add Employee",
-        "Remove Employee",
-        "Update Employee Role",
-        "Update Employee Manager",
-        "View Roles",
         "Add Role",
+        "Add Department",
       ],
     })
-    .then(function (answer) {
-      switch (answer.start) {
+    .then(function (val) {
+      switch (val.choice) {
         case "View All Employees":
-          viewAll();
+          viewAllEmployees();
           break;
 
-        case "View All Employees by Department":
-          viewDepartment();
+        case "View All Employee's By Roles":
+          viewAllRoles();
           break;
 
-        case "View All Employees by Manager":
-          viewManagers();
+        case "View all Employee's By Departments":
+          viewAllDepartments();
+          break;
+
+        case "Update Employee":
+          updateEmployee();
           break;
 
         case "Add Employee":
-          addEmployees();
-          break;
-
-        case "Remove Employee":
-         removeEmployees();
-          break;
-
-        case "Update Employee Role":
-          updateEmployees();
-          break;
-
-        case "Update Employee Manager":
-          updateMangers();
-          break;
-
-        case "View Roles":
-          viewRoles();
+          addEmployee();
           break;
 
         case "Add Role":
           addRole();
           break;
+
+        case "Add Department":
+          addDepartment();
+          break;
       }
     });
 }
 
-function viewAll() {
-
+//============= View All Employees ==========================//
+function viewAllEmployees() {
+  connection.query(
+    "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      startPrompt();
+    }
+  );
 }
 
+//============= View All Roles ==========================//
+function viewAllRoles() {
+  connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+  function(err, res) {
+  if (err) throw err
+  console.table(res)
+  startPrompt()
+  })
+}
+//============= View All Employees By Departments ==========================//
+function viewAllDepartments() {
+  connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+  function(err, res) {
+    if (err) throw err
+    console.table(res)
+    startPrompt()
+  })
+}
+
+
+//================= Select Role Quieries Role Title for Add Employee Prompt ===========//
+var roleArr = [];
+function selectRole() {
+  connection.query("SELECT * FROM role", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      roleArr.push(res[i].title);
+    }
+
+  })
+  return roleArr;
+}
+
+//============= Update Employee ==========================//
+function updateEmployee() {
+  connection.query(
+    "SELECT employee.last_name, role.title FROM employee JOIN role ON employee.role_id = role.id;",
+    function (err, res) {
+      // console.log(res)
+      if (err) throw err;
+      console.log(res);
+      inquirer
+        .prompt([
+          {
+            name: "lastName",
+            type: "rawlist",
+            choices: function () {
+              var lastName = [];
+              for (var i = 0; i < res.length; i++) {
+                lastName.push(res[i].last_name);
+              }
+              return lastName;
+            },
+            message: "What is the Employee's last name? ",
+          },
+          {
+            name: "role",
+            type: "rawlist",
+            message: "What is the Employee's new title? ",
+            choices: selectRole(),
+          },
+        ])
+        .then(function (val) {
+          var roleId = selectRole().indexOf(val.role) + 1;
+          connection.query(
+            "UPDATE employee SET role_id = ?  WHERE last_name = ?",[roleId, val.lastName],
+       
+            async function (err, res) {
+              if (err) throw err;
+              let wait = await res;
+              viewAllRoles();
+              console.table(val);
+              startPrompt();
+            }
+          );
+        });
+    }
+  );
+}
+
+//===================Add Employee Role =========================//
+
+function addRole() {
+  connection.query("SELECT ")
+}
---
+++ seed.sql
@@ -0,0 +1,35 @@
+USE employeeDB;
+INSERT INTO department (name) VALUES
+("Sales"),
+("IT"),
+("Accounting"),
+("Legal");
+
+
+
+
+
+INSERT INTO role (title, salary, department_id) VALUES
+("Lead Engineer", 150000, 2),
+("Legal Team Lead", 250000, 4),
+("Accountant", 125000, 3),
+("Sales Lead", 100000, 1),
+("Salesperson", 80000, 1),
+("Software Engineer", 120000, 2),
+("Lawyer", 190000, 4);
+
+
+
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id ) VALUES
+("Kevin", "Pulley",null,1),
+("Chad","Tarpey",null,2),
+("Alex", "Andrews",null,3),
+("Brett","Heimbach",1,4),
+("Kaitlyn","Duke",4,5),
+("Kim","Finn",1,6),
+("Kayla","Kelley",2,7);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
